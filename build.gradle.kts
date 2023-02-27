4fcd4c391347f6e103744374b98d8ca44d453606
Merlin Eden (THS) <m.eden@ths-bs.de>
2023-02-27 11:05:30 +0100

    Configure publishing to MavenCentral

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,61 @@
 plugins {
     kotlin("multiplatform") version "1.8.10"
+    id("com.vanniktech.maven.publish") version "0.24.0"
     `maven-publish`
 }
 
 group = "io.github.merlinths"
-version = "1.0.4"
+version = "1.0.0"
 
 val coroutineVersion = "1.6.4"
 val kluentVersion = "1.72"
+val mockkVersion = "1.13.4"
 
 repositories {
     mavenCentral()
-    maven("https://jitpack.io")
 }
+
+mavenPublishing {
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.Companion.S01, true)
+    signAllPublications()
+
+    coordinates(groupId = group.toString(), "swing-flow", version.toString())
+    pom {
+        name.set("SwingFlow")
+        description.set("Kotlin Flow integration for Swing.")
+
+        inceptionYear.set("2023")
+        url.set("https://github.com/merlinths/swingflow")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("merlinths")
+                name.set("Merlin Eden")
+                url.set("https://github.com/MerlinTHS/")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/MerlinTHS/SwingFlow/")
+            connection.set("scm:git:git://github.com/merlinths/swingflow.git")
+            developerConnection.set("scm:git:ssh://git@github.com/merlinths/swingflow.git")
+        }
+    }
+}
+
 
 kotlin {
     jvm {
         compilations.all {
             kotlinOptions {
-                languageVersion = "1.9"
                 freeCompilerArgs += listOf(
                     "-Xcontext-receivers",
                     "-XXLanguage:+ReferencesToSyntheticJavaProperties"
@@ -30,11 +67,10 @@ kotlin {
             useJUnitPlatform()
         }
     }
+
     sourceSets {
         val jvmMain by getting {
             dependencies {
-                implementation("com.github.MerlinTHS:Kava:1.0.2")
-
                 implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
 
                 implementation("org.slf4j:slf4j-api:2.0.6")
@@ -50,6 +86,7 @@ kotlin {
                 implementation(kotlin("test"))
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutineVersion")
                 implementation("org.amshove.kluent:kluent:$kluentVersion")
+                implementation("io.mockk:mockk:${mockkVersion}")
             }
         }
     }
